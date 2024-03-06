ceea22e8ec6eaf617624ea6f33cc2cb5edeed22c
ChromaNyan <nyancatz593@gmail.com>
2024-03-05 18:25:05 -0600

    alter conservation enchantment stats

--- src/main/java/com/chromanyan/chromaticenchantments/enchantments/ConservationEnchantment.java
+++ src/main/java/com/chromanyan/chromaticenchantments/enchantments/ConservationEnchantment.java
@@ -8,4 +8,16 @@ public class ConservationEnchantment extends Enchantment {
     public ConservationEnchantment() {
         super(Rarity.UNCOMMON, ModEnchantments.ELYTRA, new EquipmentSlot[]{EquipmentSlot.CHEST});
     }
+
+    public int getMinCost(int pEnchantmentLevel) {
+        return 5 + (pEnchantmentLevel - 1) * 8;
+    }
+
+    public int getMaxCost(int pEnchantmentLevel) {
+        return super.getMinCost(pEnchantmentLevel) + 50;
+    }
+
+    public int getMaxLevel() {
+        return 4;
+    }
 }
